44d381ee9d55770099c3ef0363e39817ba353fb5
Hilary Shaner <hilaryshaner@gmail.com>
2022-01-13 09:37:53 -0600

    Deconstructed functions and invoked

---
+++ main.js
@@ -0,0 +1,36 @@
+// 1. prepare the training pouch
+const trainingPouch = () => {
+    console.log("This is where I prepare the training pouch and treats")
+}
+
+// 2. get the leashes
+const getLeashes = () => {
+    console.log("This is when I'll grab the leashes")
+}
+
+// 3. put the leashes on
+const putOnLeashes = () => {
+    console.log("This is when I will put the leashes on both dogs")
+}
+
+// 4. walk outside and lock the door
+const lockDoor = () => {
+    console.log("Now I will walk out the front door and lock it")
+}
+
+// 5. begin walking on the sidewalk
+const walkOnSidewalk = () => {
+    console.log("Next I walk to the sidewalk to begin")
+}
+
+// 6. reward dogs when they walk next to me
+const rewardDogs = () => {
+    console.log("Now I will give each dog a treat every time they are walking calmly beside me")
+}
+
+trainingPouch()
+getLeashes()
+putOnLeashes()
+lockDoor()
+walkOnSidewalk()
+rewardDogs()
